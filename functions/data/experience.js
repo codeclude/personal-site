849051d3f603f2ae8687f8ee2437b7f4432b40f0
korczynsk1 <korczynskimm@gmail.com>
2020-01-30 00:59:09 +0100

    fix repos

--- functions/data/experience.js
+++ functions/data/experience.js
@@ -2,15 +2,15 @@ module.exports = [
     {
       company: "Deviniti",
       title: "Fullstack Developer",
-      started: "2019",
+      started: "09.2019",
       finished: "",
       seniority: ""
     },
     {
       company: "Capgemini",
       title: "Software Engineer",
-      started: "2016",
-      finished: "2019",
+      started: "06.2016",
+      finished: "09.2019",
       seniority: ""
     }
 ];
